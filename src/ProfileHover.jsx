dafcbc8701dac3a5f1fd053a3824eadca745160a
Oskar Nyberg <oskar@oskarnyberg.com>
2019-05-04 13:57:54 +0200

    Added minimal react component and updated webpack config to compile both sources

---
+++ src/ProfileHover.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export class ProfileHover extends React.Component {
+  render() {
+    return '3box-profile';
+  }
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,8 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = {
-  entry: './src/widget.js',
-  output: {
-    filename: 'widget.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+const createConfig = ({ entry, output, babelOptions }) => ({
+  entry,
+  output,
   watch: true,
   module: {
     rules: [
@@ -13,45 +10,70 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
         resolve: {
-          extensions: ['.js', '.jsx']
+          extensions: [".js", ".jsx"]
         },
-        use: {
-          loader: 'babel-loader',
-          options: {
-            babelrc: false,
-            presets: ['@babel/preset-env'],
-            plugins: [
-              ['@babel/plugin-syntax-jsx'],
-              ['@babel/plugin-transform-react-jsx', { pragma: 'dom' }],
-              ['@babel/plugin-transform-runtime', { regenerator: true }],
-              ['@babel/plugin-proposal-object-rest-spread']
-            ]
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              babelrc: false,
+              ...babelOptions
+            }
           }
-        }
+        ]
       },
       {
-      test: /\.less$/,
-      use: [
-        {
-          loader: "css-loader",
-          options: {
-            sourceMap: true,
-            modules: true,
-            localIdentName: "threeboxProfileHover__[name]_[local]"
+        test: /\.less$/,
+        use: [
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: "threeboxProfileHover__[name]_[local]"
+            }
+          },
+          {
+            loader: "less-loader"
           }
-        },
-        {
-          loader: "less-loader"
-        }
-      ]
-    }
+        ]
+      }
     ]
-  },
-  node: {
-    console: false,
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty'
   }
-};
+});
+
+const nonReactConfig = createConfig({
+  entry: "./src/widget.js",
+  output: {
+    filename: "widget.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  babelOptions: {
+    presets: ["@babel/preset-env"],
+    plugins: [
+      ["@babel/plugin-syntax-jsx"],
+      ["@babel/plugin-transform-react-jsx", { pragma: "dom" }],
+      ["@babel/plugin-transform-runtime", { regenerator: true }],
+      ["@babel/plugin-proposal-object-rest-spread"]
+    ]
+  }
+});
+
+const reactConfig = createConfig({
+  entry: "./src/ProfileHover.jsx",
+  output: {
+    library: "ProfileHover",
+    libraryTarget: "umd",
+    filename: "reactBundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  babelOptions: {
+    presets: ["@babel/preset-env", "@babel/preset-react"],
+    plugins: [
+      ["@babel/plugin-transform-react-jsx"],
+      ["@babel/plugin-transform-runtime", { regenerator: true }]
+    ]
+  }
+});
+
+module.exports = [nonReactConfig, reactConfig];
