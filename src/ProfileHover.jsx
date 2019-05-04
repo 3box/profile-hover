c0e3a3ad7e794a484ce8b2cc9d4c4cc8fdf7eee2
Oskar Nyberg <oskar@oskarnyberg.com>
2019-05-04 18:54:21 +0200

    React component now fetching profile data

--- src/ProfileHover.jsx
+++ src/ProfileHover.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import { getProfile, getVerifiedAccounts } from "3box/lib/api";
 
 export default class ProfileHover extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      profile: null,
+      verified: null
+    };
+  }
+
+  async componentDidMount() {
+    const {
+      address,
+    } = this.props;
+
+    const profile = await getProfile(address);
+    const verified = await getVerifiedAccounts(profile);
+    this.setState({ profile, verified });
+  }
+
   render() {
-    return '3box-profile';
+    const {
+      address,
+      display,
+      theme
+    } = this.props;
+
+    const {
+      profile,
+      verified,
+    } = this.state;
+
+    console.log({ profile, verified });
+
+    return null;
   }
 }
 
