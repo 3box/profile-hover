298b62043e73a08f0f4d24ebf34cace99eab4f58
Oskar Nyberg <oskar@oskarnyberg.com>
2019-05-04 18:46:03 +0200

    Expanded react example with more addresses and options

--- example/app.js
+++ example/app.js
@@ -2,7 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ProfileHover from '../src/ProfileHover';
 
+const Example = ({}) => {
+  return (
+    <>
+      <div className='ethAddress'>
+        <ProfileHover address='0xE3b281F3Dd2b87c7b3EAcC0402a4FD7d827F2956' />
+      </div>
+      <div className='ethAddress'>
+        <ProfileHover address='0xf3b281F3Dd2b87c7b3EAcC0402a4FD7d827F2956' />
+      </div>
+      <div className='ethAddress'>
+        <ProfileHover address='0xa8ee0babe72cd9a80ae45dd74cd3eae7a82fd5d1' />
+      </div>
+      <div className='ethAddress'>
+        <ProfileHover address='0x258ddd84abf61ba1d1e39f95d8863ee9ca218c06' />
+      </div>
+      <div className='ethAddress'>
+        <ProfileHover address='0xbaebb7d18f8b16b0a970fda91f1efa626d67423e' />
+      </div>
+      <div className='ethAddress'>
+        <ProfileHover address='0xff326878d13b33591d286372e67b4af05cd100bd' />
+      </div>
+      <div className='ethAddress'>
+        <ProfileHover address='0xa8ee0babe72cd9a80ae45dd74cd3eae7a82fd5d1' display='full' />
+      </div>
+      <div className='ethAddress'>
+        <ProfileHover address='0xa8ee0babe72cd9a80ae45dd74cd3eae7a82fd5d1' theme='none'>
+          0xa8ee0babe72cd9a80ae45dd74cd3eae7a82fd5d1
+        </ProfileHover>
+      </div>
+    </>
+  )
+}
+
 const appContainer = document.getElementById('reactApp');
-const component = <ProfileHover />;
-ReactDOM.render(component, appContainer);
+ReactDOM.render(<Example />, appContainer);
 
