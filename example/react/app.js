6b7b02b0d3f5b05d92716aaff24f5e5fc7df8f79
Kenzo <presto12321@gmail.com>
2019-05-30 12:41:05 -0400

    fix mobile css

--- example/react/app.js
+++ example/react/app.js
@@ -6,28 +6,28 @@ const Example = ({}) => {
   return (
     <>
       <div className='ethAddress'>
-        <ProfileHover orientation="top" url="www.yahoo.com" address='0x9ACb0539f2ea0C258aC43620Dd03eF01f676a69b' />
+        <ProfileHover orientation="bottom" url="www.yahoo.com" address='0x9ACb0539f2ea0C258aC43620Dd03eF01f676a69b' />
       </div>
       <div className='ethAddress'>
-        <ProfileHover orientation="top" noImgs address='0xf3b281F3Dd2b87c7b3EAcC0402a4FD7d827F2956' />
+        <ProfileHover orientation="bottom" noImgs address='0xf3b281F3Dd2b87c7b3EAcC0402a4FD7d827F2956' />
       </div>
       <div className='ethAddress'>
-        <ProfileHover orientation="top" noCoverImg address='0xa8ee0babe72cd9a80ae45dd74cd3eae7a82fd5d1' />
+        <ProfileHover orientation="bottom" noCoverImg address='0xa8ee0babe72cd9a80ae45dd74cd3eae7a82fd5d1' />
       </div>
       <div className='ethAddress'>
-        <ProfileHover orientation="top" noCoverImg address='0x5c44e8982fa3c3239c6e3c5be2cc6663c7c9387e' />
+        <ProfileHover orientation="bottom" noCoverImg address='0x5c44e8982fa3c3239c6e3c5be2cc6663c7c9387e' />
       </div>
       <div className='ethAddress'>
-        <ProfileHover orientation="top" noImgs address='0xbaebb7d18f8b16b0a970fda91f1efa626d67423e' />
+        <ProfileHover orientation="bottom" noImgs address='0xbaebb7d18f8b16b0a970fda91f1efa626d67423e' />
       </div>
       <div className='ethAddress'>
-        <ProfileHover orientation="top" address='0xff326878d13b33591d286372e67b4af05cd100bd' />
+        <ProfileHover orientation="bottom" address='0xff326878d13b33591d286372e67b4af05cd100bd' />
       </div>
       <div className='ethAddress'>
-        <ProfileHover orientation="top" noImgs address='0xa8ee0babe72cd9a80ae45dd74cd3eae7a82fd5d1' fullDisplay />
+        <ProfileHover orientation="bottom" noImgs address='0xa8ee0babe72cd9a80ae45dd74cd3eae7a82fd5d1' fullDisplay />
       </div>
       <div className='ethAddress'>
-        <ProfileHover orientation="top" address='0xa8ee0babe72cd9a80ae45dd74cd3eae7a82fd5d1' noTheme>
+        <ProfileHover orientation="bottom" address='0xa8ee0babe72cd9a80ae45dd74cd3eae7a82fd5d1' noTheme>
           0xa8ee0babe72cd9a80ae45dd74cd3eae7a82fd5d1
         </ProfileHover>
       </div>
